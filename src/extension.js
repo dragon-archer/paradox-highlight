d82131bcee9e50bf8febb8b2d61f2bf6e90791ba
dragon-archer <dragon-archer@outlook.com>
2024-10-25 16:40:50 +0800

    fix(JS): Enable formatting for EU4

--- src/extension.js
+++ src/extension.js
@@ -99,11 +99,13 @@ function activate(context) {
 
 	// Register the document formatting provider
 	vscode.languages.registerDocumentFormattingEditProvider('ck3', documentFormatter);
+	vscode.languages.registerDocumentFormattingEditProvider('eu4', documentFormatter);
 	vscode.languages.registerDocumentFormattingEditProvider('vic3', documentFormatter);
 	vscode.languages.registerDocumentFormattingEditProvider('paradox', documentFormatter);
 
 	// Register the document range formatting provider
 	vscode.languages.registerDocumentRangeFormattingEditProvider('ck3', rangeFormatter);
+	vscode.languages.registerDocumentRangeFormattingEditProvider('eu4', rangeFormatter);
 	vscode.languages.registerDocumentRangeFormattingEditProvider('vic3', rangeFormatter);
 	vscode.languages.registerDocumentRangeFormattingEditProvider('paradox', rangeFormatter);
 
